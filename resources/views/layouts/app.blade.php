4ed979ece3ead13d61928f3d4626ed1ab6a07b6d
Omar Yehia <oyehia94@gmail.com>
2021-03-22 12:28:33 +0200

    Applied authentication on all routes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -24,7 +24,7 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ route('posts.index') }}">
                     Blog
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -54,9 +54,13 @@
                             @endif
                         @else
                             <li class="nav-item dropdown">
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('posts.index') }}">All Posts</a>
+                                </li>
                                 <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
                                     {{ Auth::user()->name }}
                                 </a>
+                                
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                     <a class="dropdown-item" href="{{ route('logout') }}"
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get("/posts", [PostsController::class, "index"])->name("posts.index");
-Route::get("/posts/create", [PostsController::class, "create"])->name("posts.create");
-Route::post("posts", [PostsController::class, "store"])->name("posts.store");
-Route::get("/posts/restore", [PostsController::class, "restoreDeleted"])->name("posts.restore");
-Route::get("/posts/{post}", [PostsController::class, "show"])->name("posts.show");
-Route::get("/posts/{post}/edit", [PostsController::class, "edit"])->name("posts.edit");
-Route::put("/posts/{post}", [PostsController::class, "update"])->name("posts.update");
-Route::delete("/posts/{post}", [PostsController::class, "destroy"])->name("posts.destroy");
-
-Route::post("/comments", [CommentsController::class, "store"])->name("comments.store");
+Route::get("/", function () {
+    return redirect()->route("posts.index");
+});
+Route::group(['middleware' => ['auth']], function () {
+    Route::get("/posts", [PostsController::class, "index"])->name("posts.index");
+    Route::get("/posts/create", [PostsController::class, "create"])->name("posts.create");
+    Route::post("posts", [PostsController::class, "store"])->name("posts.store");
+    Route::get("/posts/restore", [PostsController::class, "restoreDeleted"])->name("posts.restore");
+    Route::get("/posts/{post}", [PostsController::class, "show"])->name("posts.show");
+    Route::get("/posts/{post}/edit", [PostsController::class, "edit"])->name("posts.edit");
+    Route::put("/posts/{post}", [PostsController::class, "update"])->name("posts.update");
+    Route::delete("/posts/{post}", [PostsController::class, "destroy"])->name("posts.destroy");
+    Route::post("/comments", [CommentsController::class, "store"])->name("comments.store");
+});
 
 Auth::routes();
 
