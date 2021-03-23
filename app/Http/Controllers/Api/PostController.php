fb7e4cb28c560e1b4858c3d7fa94d1bb0abedb2c
Omar Yehia <oyehia94@gmail.com>
2021-03-23 13:41:30 +0200

    added store method for API

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StorePostRequest;
 use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
@@ -18,5 +19,17 @@ class PostController extends Controller
     public function show(Post $post)
     {
         return new PostResource($post);
+    }
+
+    public function store(StorePostRequest $request)
+    {
+        $post = new Post;
+        $post->title = $request->title;
+        $post->user_id = $request->user_id;
+        $post->description = $request->description;
+        $res = $post->save();
+        if ($res) {
+            return [$post];
+        }
     }
 }
